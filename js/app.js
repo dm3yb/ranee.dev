5b94771a5a2cc8380fccba70ef9b38de7ab5a348
Ranee <67798875+RaneeDEV@users.noreply.github.com>
2021-05-18 18:37:55 +0300

    Section "Portfolio"

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const accordionEl = document.getElementById("accordion");
+
+accordionEl.addEventListener("click", (e) => {
+  const title = e.target.closest(".accordion-title");
+  const collapse = title.nextElementSibling;
+  const content = collapse.firstElementChild;
+  if (collapse.classList.contains("active")) {
+    collapse.style.height = 0;
+    collapse.classList.remove("active");
+    title.classList.remove('active-title')
+  } else {
+    collapse.style.height = getComputedStyle(content).height;
+    collapse.classList.add("active");
+    title.classList.add('active-title')
+  }
+});
